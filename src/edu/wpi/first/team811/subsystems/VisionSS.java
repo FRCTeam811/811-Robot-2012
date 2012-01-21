3c09e5e1136917bcb9b73f87d973be33682ff567
saswat <saswat@ubuntu.ubuntu-domain>
2012-01-21 16:36:26 -0500

    Initial backup

---
+++ src/edu/wpi/first/team811/subsystems/VisionSS.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.team811.subsystems;
+
+import edu.wpi.first.team811.SubSystem;
+import edu.wpi.first.team811.Team811Robot;
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+
+/**
+ *
+ * @author saswat
+ */
+public class VisionSS extends SubSystem {
+
+    public VisionSS(Team811Robot teamrobot) {
+        super(teamrobot);
+    }
+    
+    public String ID() {
+        return "VisionSS";
+    }
+
+    public void logic(Object param) {
+        d.ac.freshImage();
+        d.rd1.arcadeDrive((0.75) * d.joy1.getY(), 0.75 * d.joy1.getThrottle(), true);
+    }
+}
